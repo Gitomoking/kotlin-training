f75424d6064b1d2ae9ee2d9c6ebbacfdd1bbbc89
mizogami <tomochuri25@gmail.com>
2018-06-23 23:31:51 +0900

    Add primary constructor Person2

--- person.kt
+++ person.kt
@@ -19,10 +19,22 @@ class Person(name: String) {
   }
 }
 
+class Person2(val firstName: String, val lastName: String = "") {
+  fun print() {
+    println("お名前: ${firstName} ${lastName}")
+  }
+}
+
 fun main(args: Array<String>) {
   val person = Person("Tomoki")
   person.print()
 
   val person2 = Person("Tomoki", "Mizogami")
   person2.print()
+
+  val person3 = Person2("Tomoki")
+  person3.print()
+
+  val person4 = Person2("Tomoki", "Mizogami")
+  person4.print()
 }
